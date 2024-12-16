0125099fa76b90d6f91fe855d886c9752ba0bc5f
Aspirant ghislain <ghislainmundeke0@gmail.com>
2024-12-17 00:31:18 +0100

    ghg

---
+++ nav/nav.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="../style.css">
+<div id="nav_princ">
+                
+     <nav>
+     <li><a href="login.php" title="Aller vers">Actualité</a></li>
+     <li><a href="index.php">Accueil</a></li>
+    </nav>
